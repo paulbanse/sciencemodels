318526d89d16e146ed61b183f70ad8ad5349f802
paulbanse <paul.banse@ens-rennes.fr>
2025-09-03 12:53:36 +0200

    added a requirement file

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # sciencemodels
 Here is a project to model scientific exploration
+
+## To run:
+install needed dependencies via pip using requirement.txt file (generated with pipreqs)
+use python to run runner.py
+
--- mesa_model.py
+++ mesa_model.py
@@ -6,7 +6,6 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import itertools
-import landscapefunctions
 from matplotlib import cm
 from matplotlib.ticker import LinearLocator
 # self.model.grid.move_agent(self, new_position)
@@ -312,23 +311,3 @@ class MyModel(mesa.Model):
         plt.show()
 
 
-generation_params = {'seed': 1, 
-                     "std": 15, 
-                     "number_gaussians": 5}
-        # landscapefunctions.multipleGaussians
-all_params = {'n_agents':3,             # minimum 3
-              "n_connection": 3,
-              "initial_curiosity":0.4 , # interest for exploration
-              "epsilon": 0.1,           # noise in the measure of utility
-              "harvest":0.1,            # proportion of science harvested at each iteration 
-              "sizeGrid":10,
-              "initCellFunc":landscapefunctions.randominit, # generation method for the landscape, see landscapefunctions for the definitions
-              "generation_params": generation_params,       # parameters for the generation method
-              "agent_generation_rate": 0,                       # number of agent generated per generation, for example if 0.1, 10 new agent per generation
-              "constant_population": 0                      # NOT YET IMPLEMENTED delete as many agent as generate by age for a constant population
-              }
-model = MyModel(**all_params)
-
-
-model.animate_steps()
-#model.Vizualize_3d()
