05892c8583ef3c885a8e828112aed93aab21517e
paulbanse <paul.banse@ens-rennes.fr>
2025-10-20 14:30:05 +0200

    created a bulk runner and a pipeline for basic data analysis, some code clean up will be needed

---
+++ bulk_runner.py
@@ -0,0 +1,28 @@
+
+import mesa_model
+import landscapefunctions
+import os
+
+generation_params = {'seed': 4}
+"""example of generation params for landscapefunctions.multipleGaussians : 
+
+generation_params = {'seed': 4, 
+                     "std": 5, 
+                     "number_gaussians": 2}
+"""
+all_params = {'n_agents':20,             # minimum 3
+              "n_connection": 2,
+              "initial_curiosity":[k/10 for k in range(11)], # interest for exploration
+              "use_distance": False,        # if True, agents take into account distance when choosing where to go
+              "epsilon": 0.1,           # noise in the measure of utility
+              "harvest":0.02,            # proportion of science harvested at each iteration 
+              "sizeGrid":20,
+              "initCellFunc":landscapefunctions.randominit, # generation method for the landscape, see landscapefunctions for the definitions
+              "generation_params": generation_params,       # parameters for the generation method
+              "agent_generation_rate": 0,                       # number of agent generated per generation, for example if 0.1, 10 new agent per generation
+              "constant_population": 0,                     # NOT YET IMPLEMENTED delete as many agent as generate by age for a constant population
+              "agent_seed": 1,                           # seed for the random of the agents
+              "step_limit": 400
+              }
+
+mesa_model.generate_data_parametric_exploration("end_report.csv", param_grid = all_params, repeats_per_setting = 40, change_landscape_seed = True, intention = "w", skip_to = 0)
